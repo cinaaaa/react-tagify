e44b2b6ed04a9796ac9ab2cb0c7445b5c12c4996
E-RROR <sina.farhadi.cyber@gmail.com>
2019-12-12 17:07:18 +0330

    ADD MAIN Component

---
+++ src/ReactTagify.component.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ReactTagify = ({children, colors, tagClicked}) => {
+
+    // Childs Maked Array
+    const childs = React.Children.toArray((children));
+
+    // Html Element
+    const TempSpan = (props) => {
+        return (
+            <span
+                style={{
+                    color: props.color,
+                    cursor: props.tagClicked ? 'pointer':'default',
+                }}
+                onClick={() => {
+                    if (props.tagClicked) {
+                        props.tagClicked(props.text.substr(1));
+                    }
+                    else {
+                        //
+                    }
+                }}
+            >
+                <span>{props.text}</span>
+            </span>
+        )
+    };
+
+
+    return (
+        <span>
+            { children &&
+                <span>
+                    {childs.map((i) => {
+
+                        // Handle Inputs and Return at End
+                        let input = i;
+
+                        // Child Is Element
+                        if (input.props) {
+                            let splitted = input.props.children.split(" ");
+                            let tagified_text = [' ']
+
+                            splitted.forEach((text) => {
+                                if (text.match(/#[a-z0-9_]+/g) && !text.match(/@[a-z0-9_]+/g)){
+                                    // Temp Child to Replace with last one
+                                    tagified_text.push(
+                                        <TempSpan
+                                            text={text}
+                                            color={colors}
+                                            hashtagClicked={tagClicked}
+                                            key={Math.floor(Math.random() * 9999999)}
+                                        />
+                                    );
+
+                                    // Push Space
+                                    tagified_text.push(' ');
+                                }
+                                else if (!text.match(/@[a-z0-9_]+/g)) {
+                                    // Push Non Hashtags texts
+                                    tagified_text.push(text)
+
+                                    // Push Space
+                                    tagified_text.push(' ');
+                                }
+                            });
+
+                            splitted.forEach((text) => {
+                                if (text.match(/@[a-z0-9_]+/g)){
+
+                                    // Temp Child to Replace with last one
+                                    tagified_text.push(
+                                        <TempSpan
+                                            text={text}
+                                            color={colors}
+                                            tagClicked={tagClicked}
+                                            key={Math.floor(Math.random() * 9999999)}
+                                        />
+                                    );
+
+                                    // Push Space
+                                    tagified_text.push(' ');
+                                }
+                            });
+
+                            // Change Target Element in Input
+                            let edited_component = React.cloneElement(i, {
+                                children: tagified_text,
+                            });
+
+                            input = edited_component;
+                        };
+
+                        // Child Is String Just
+                        if (!input.props) {
+                            let splitted = input.split(" ");
+                            let tagified_text = []
+                            let temp_element = <span>temp</span>
+
+                            splitted.forEach((text) => {
+                                if (text.match(/#[a-z0-9_]+/g)){
+                                    // Temp Child to Replace with last one
+                                    tagified_text.push(
+                                        <TempSpan
+                                            text={text}
+                                            color={colors}
+                                            tagClicked={tagClicked}
+                                            key={Math.floor(Math.random() * 9999999)}
+                                        />
+                                    );
+
+                                    // Push Space
+                                    tagified_text.push(' ');
+                                }
+                                else {
+                                    // Push Non Tags texts
+                                    tagified_text.push(text)
+
+                                    // Push Space
+                                    tagified_text.push(' ');
+                                }
+                            });
+
+                            // Change Target Element in Input
+                            let edited_component = React.cloneElement(temp_element, {
+                                children : tagified_text,
+                            });
+
+                            input = edited_component;
+                        };
+
+                        // Return Final Result
+                        return (
+                            <span
+                                key={Math.floor(Math.random() * 9999999)}
+                            >
+                                {input}
+                            </span>
+                        )
+                    })}
+                </span>
+            }
+        </span>
+  );
+};
+
+ReactTagify.propTypes = {
+    colors: PropTypes.string,
+    tagClicked: PropTypes.func
+};
+ReactTagify.defaultProps = {
+    colors: '#0073e6',
+};
