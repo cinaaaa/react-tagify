a34e05ec1d186e373098c0834e9076a054b09657
Abdelrahman Ashraf <a.theashraf@gmail.com>
2020-01-02 23:19:34 +0800

    test: 💍 adding unit test

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-var path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/ReactTagify.component.jsx',
-    output: {
-        path: path.resolve('lib'),
-        filename: 'ReactTagify.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            }
-        ]
-    }
-}
+  mode: "production",
+  entry: "./src/ReactTagify.component.jsx",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "ReactTagify.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: [{ loader: "babel-loader", options: { babelrc: true } }]
+      }
+    ]
+  }
+};
