dfd02f80fdc65c08eb7feb22da2aa72fd8cc50fa
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-04-01 01:34:05 +0330

    feat: update and lint the files

--- packages/react-tagify/src/helpers/matchers.ts
+++ packages/react-tagify/src/helpers/matchers.ts
@@ -1,24 +1,24 @@
 type getMatchedTypeProps = {
-    text: string;
-    detectHashtags: boolean;
-    detectMentions: boolean;
+  text: string;
+  detectHashtags: boolean;
+  detectMentions: boolean;
 };
 
 export const getMatchedType = (args: getMatchedTypeProps) => {
-    const matchers = [
-        { type: 'hashtag', pattern: /#[a-zA-Z0-9_]+/g },
-        { type: 'mention', pattern: /@[a-zA-Z0-9_]+/g }
-    ];
+  const matchers = [
+    { type: 'hashtag', pattern: /#[a-zA-Z0-9_]+/g },
+    { type: 'mention', pattern: /@[a-zA-Z0-9_]+/g },
+  ];
 
-    for (const matcher of matchers) {
-        if (matcher.pattern.test(args.text)) {
-            if (
-                (matcher.type === 'hashtag' && args.detectHashtags) ||
-                (matcher.type === 'mention' && args.detectMentions)
-            ) {
-                return matcher.type;
-            }
-        }
+  for (const matcher of matchers) {
+    if (matcher.pattern.test(args.text)) {
+      if (
+        (matcher.type === 'hashtag' && args.detectHashtags) ||
+        (matcher.type === 'mention' && args.detectMentions)
+      ) {
+        return matcher.type;
+      }
     }
-    return null;
+  }
+  return null;
 };
--- packages/react-tagify/src/helpers/sign-remover.ts
+++ packages/react-tagify/src/helpers/sign-remover.ts
@@ -1,6 +1,5 @@
-// remove # and @ from texts
 export const signRemover = (str: string) => {
-    if (str.startsWith('#')) return str.slice(1);
-    if (str.startsWith('@')) return str.slice(1);
-    return str;
+  if (str.startsWith('#')) return str.slice(1);
+  if (str.startsWith('@')) return str.slice(1);
+  return str;
 };
